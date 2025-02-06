ae625d2f24cb83bbf55ba3ca042f229a97bd7420
Samsegun <oyebadesegunsam@gmail.com>
2025-02-06 15:02:28 +0100

    responsive styling for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,25 +2,37 @@ import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
 import Logo from "../ui/Logo";
 import Heading from "../ui/Heading";
+import { breakpoints } from "../styles/breakpoints";
 
 const LoginLayout = styled.main`
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 48rem;
-  align-content: center;
-  justify-content: center;
-  gap: 3.2rem;
-  background-color: var(--color-grey-50);
+    min-height: 100vh;
+    display: grid;
+    grid-template-columns: 320px;
+    align-content: center;
+    justify-content: center;
+    gap: 3.2rem;
+    background-color: var(--color-grey-50);
+
+    ${breakpoints.tablet} {
+        grid-template-columns: 48rem;
+    }
+`;
+
+const LogoWrapper = styled.div`
+    display: flex;
+    justify-content: center;
 `;
 
 function Login() {
-  return (
-    <LoginLayout>
-      <Logo />
-      <Heading as="h4">Log in to your account</Heading>
-      <LoginForm />
-    </LoginLayout>
-  );
+    return (
+        <LoginLayout>
+            <LogoWrapper>
+                <Logo />
+            </LogoWrapper>
+            <Heading as='h4'>Log in to your account</Heading>
+            <LoginForm />
+        </LoginLayout>
+    );
 }
 
 export default Login;
